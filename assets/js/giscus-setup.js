5a4159d548b9795806a87142c8c5587f7f663ba8
fz920 <97394947+fz920@users.noreply.github.com>
2026-05-11 13:02:02 +0000

    Deploying to gh-pages from @ fz920/fz920.github.io@db7f261604be4fe26a832ea819036ac02d9ccfe7 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "fz920/fengzhezhang.github.io",
+    "data-repo": "fz920/fz920.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
